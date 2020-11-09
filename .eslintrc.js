83e122c3420630e29ed77b7e8842122843ee2e14
Mikko Ojala <mikko.j.ojala@aalto.fi>
2020-11-09 11:10:03 +0200

    Initial commit
    Run expo init with tabs-typescript template. Config ESLint and
    prettier. Add a husky pre-commit webhook for prettier format and eslint.

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'universe/native',
+    'universe/web',
+    'universe/shared/typescript-analysis',
+  ],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx', '*.d.ts'],
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+  rules: {
+    'no-console': ['error', { allow: ['warn'] }],
+  },
+}
