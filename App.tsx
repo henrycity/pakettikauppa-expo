a274c87f0cef12ace665b382a85a003805a1cfde
Mikko Ojala <mikko.j.ojala@aalto.fi>
2020-11-17 11:15:36 +0200

    Add SWR configuration
    Use a different fetcher for native and web (native fetcher sends
    	authorization header).

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar'
 import React from 'react'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
 
+import ConfigSWR from './components/ConfigSWR'
 import useCachedResources from './hooks/useCachedResources'
 import useColorScheme from './hooks/useColorScheme'
 import Navigation from './navigation'
@@ -15,8 +16,10 @@ export default function App(): null | JSX.Element {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
+        <ConfigSWR>
+          <Navigation colorScheme={colorScheme} />
+          <StatusBar />
+        </ConfigSWR>
       </SafeAreaProvider>
     )
   }
---
+++ components/ConfigSWR/ConfigSWRNative.tsx
@@ -0,0 +1,52 @@
+import * as SecureStore from 'expo-secure-store'
+import React, { useState, useEffect } from 'react'
+import { SWRConfig } from 'swr'
+
+const serverUrl = 'http://192.168.100.11:3000'
+
+const nativeFetcherOptions = (token: string | null) => {
+  if (token)
+    return {
+      credentials: 'include' as const,
+      headers: {
+        Authorization: 'Bearer ' + token,
+      },
+    }
+  else
+    return {
+      credentials: 'include' as const,
+    }
+}
+
+function getNativeFetcher(token: string | null) {
+  return (url: string) =>
+    fetch(serverUrl + url, nativeFetcherOptions(token)).then((res) =>
+      res.json()
+    )
+}
+
+export default function ConfigSWRNative({
+  children,
+}: {
+  children: JSX.Element | JSX.Element[]
+}) {
+  const [token, setToken] = useState<string | null>(null)
+  const [loaded, setLoaded] = useState(false)
+
+  useEffect(() => {
+    SecureStore.getItemAsync('idToken').then((token) => {
+      setToken(token)
+      setLoaded(true)
+    })
+  }, [])
+
+  return loaded ? (
+    <SWRConfig
+      value={{
+        fetcher: getNativeFetcher(token),
+      }}
+    >
+      {children}
+    </SWRConfig>
+  ) : null
+}
