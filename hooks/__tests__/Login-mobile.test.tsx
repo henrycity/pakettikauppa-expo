1e486ec228b5bd9280c314f7c4689a1d75446e50
Pyry Pohjalainen <pyry@MacBook-Pro.local>
2020-12-14 14:59:32 +0200

    first test done

--- hooks/__tests__/Login-mobile.test.tsx
+++ hooks/__tests__/Login-mobile.test.tsx
@@ -1,14 +1,9 @@
-// Work in progress
-
-import { render, fireEvent, cleanup } from '@testing-library/react-native'
+import { render, cleanup, waitFor } from '@testing-library/react-native'
 import React from 'react'
-import { debug } from 'react-native-reanimated'
+import { debug, exp } from 'react-native-reanimated'
 
 import Navigation from '../../navigation/index'
-
-// import Navigation from '../../navigation/index'
-// import RootNavigator from '../../navigation/index'
-import LoginScreen from '../../screens/LoginScreen'
+// import LoginScreen from '../../screens/LoginScreen'
 import useUser from '../useUser'
 
 describe('Testing mobile authorization', () => {
@@ -18,26 +13,30 @@ describe('Testing mobile authorization', () => {
 
   jest.mock('../useUser')
   jest.mock('../useLogin')
-
   it('login page should open when isLoggedIn is false', async () => {
     process.env.TEST_ENV = 'mobile'
-    const { getByText, debug } = render(comp)
+    jest.mock('../useUser', () => ({ user: 'aa', isLoggedIn: false }))
     const { isLoggedIn } = useUser()
-    // console.log(isLoggedIn)
     expect(isLoggedIn).toEqual(false)
-    // const loginText = await getByText('Login or register with a Google account')
-    // console.log(loginText)
-    // expect(loginText).toBeTruthy()
+    const { getByText } = render(comp)
+    await waitFor(() => {
+      const LoginScreen = getByText('Login or register with a Google account')
+      expect(LoginScreen).toBeTruthy()
+    })
   })
 
-  /**it('login page should come when isLoggedIn is false', async () => {
+  /** 
+  jest.mock('../useUser')
+    jest.mock('../useLogin')
+  it('Profile page should open when isLoggedIn is true', async () => {
     process.env.TEST_ENV = 'mobile'
-    const { getByText } = render(comp)
     const { isLoggedIn } = useUser()
     expect(isLoggedIn).toEqual(true)
-    const loginText = await getByText('Profile Screen!')
-    console.log(loginText)
-    expect(loginText).toBeTruthy()
+    const { getByText } = render(comp)
+    await waitFor(() => {
+      const ProfileScreen = getByText('Profile Screen!')
+      expect(ProfileScreen).toBeTruthy()
+    })
   })*/
 })
 
