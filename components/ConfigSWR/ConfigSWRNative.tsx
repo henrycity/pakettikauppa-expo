566130fe308edc9870bccbe1423e4f0cece83e62
Veeti Poutsalo <veeti@poutsalo.com>
2020-12-15 15:52:54 +0200

    Change deprecated AsyncStorage to correct dependency

--- components/ConfigSWR/ConfigSWRNative.tsx
+++ components/ConfigSWR/ConfigSWRNative.tsx
@@ -1,5 +1,5 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import React from 'react'
-import { AsyncStorage } from 'react-native'
 import { SWRConfig } from 'swr'
 
 import options from './options'
--- hooks/useLogout.tsx
+++ hooks/useLogout.tsx
@@ -1,4 +1,5 @@
-import { AsyncStorage, Platform } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Platform } from 'react-native'
 import { mutate } from 'swr'
 
 export default function useLogout(): Function {
