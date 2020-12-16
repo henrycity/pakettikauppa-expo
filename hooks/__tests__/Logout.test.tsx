8cd4811b2bf73b62eb5f51aad573e1397fb9563d
Veeti Poutsalo <veeti@poutsalo.com>
2020-12-16 16:42:01 +0200

    Finish log out button tests

--- hooks/__tests__/Logout.test.tsx
+++ hooks/__tests__/Logout.test.tsx
@@ -1,17 +1,26 @@
-import { render, cleanup, waitFor } from '@testing-library/react-native'
+import {
+  render,
+  cleanup,
+  waitFor,
+  fireEvent,
+  act,
+} from '@testing-library/react-native'
 import React from 'react'
 
 import Navigation from '../../navigation/index'
+import useLogout from '../useLogout'
 import useUser from '../useUser'
 
+const mockLogout = jest.fn()
 jest.mock('../useUser', () => jest.fn())
+jest.mock('../useLogout', () => jest.fn())
+useLogout.mockImplementation(() => () => mockLogout)
 
 describe('Log out button', () => {
   afterEach(cleanup)
 
   it('should not be visible when not logged in', async () => {
     useUser.mockImplementation(() => ({ user: 'test', isLoggedIn: false }))
-    process.env.TEST_ENV = 'web'
     const { queryByText } = render(<Navigation colorScheme="light" />)
     const { isLoggedIn } = useUser()
     expect(isLoggedIn).toEqual(false)
@@ -22,12 +31,23 @@ describe('Log out button', () => {
 
   it('should be visible when logged in', async () => {
     useUser.mockImplementation(() => ({ user: 'test', isLoggedIn: true }))
-    process.env.TEST_ENV = 'web'
     const { getByText } = render(<Navigation colorScheme="light" />)
     const { isLoggedIn } = useUser()
     expect(isLoggedIn).toEqual(true)
     await waitFor(() => {
       expect(getByText('Log out')).toBeTruthy()
+    })
+  })
+
+  it('should call logout when pressed', async () => {
+    useUser.mockImplementation(() => ({ user: 'test', isLoggedIn: true }))
+    const { getByText } = render(<Navigation colorScheme="light" />)
+    const { isLoggedIn } = useUser()
+    expect(isLoggedIn).toEqual(true)
+    await waitFor(() => {
+      const button = getByText('Log out')
+      act(fireEvent.press(button))
+      expect(mockLogout).toHaveBeenCalled()
     })
   })
 })
