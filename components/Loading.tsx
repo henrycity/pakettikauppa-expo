3c0e9e4fa3b7e318ff1d855fe49c1aadb14b877e
Veeti Poutsalo <veeti@poutsalo.com>
2021-01-05 16:49:56 +0200

    Add text to loading component
    ActivityIndicator isn't visible on all devices, causing the loading
    component to be completely empty. Adding some text fixes this issue.

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { ActivityIndicator, StyleSheet } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text } from 'react-native'
 
 import { View } from './Themed'
 
 export default function Loading(): JSX.Element {
   return (
     <View style={styles.container}>
+      <Text>Loading...</Text>
       <ActivityIndicator />
     </View>
   )
