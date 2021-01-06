f3f9ffa568a942a875f6ef928fbd257c74c1e08d
Veeti Poutsalo <veeti@poutsalo.com>
2021-01-06 17:35:07 +0200

    Implement tests for logout

---
+++ cypress/integration/logout_spec.js
@@ -0,0 +1,33 @@
+describe('Navigation drawer', () => {
+  beforeEach(() => {
+    cy.request('POST', 'http://localhost:3000/login', 'testToken')
+    cy.visit('/')
+    cy.waitForReact()
+  })
+
+  it('should have log out button', () => {
+    cy.react('MenuButton').click()
+    cy.react('DrawerItem', { props: { label: 'Log out' } }).click()
+    cy.getCookie('jwt').should('have.property', 'value', '')
+  })
+})
+
+describe('Log out button', () => {
+  beforeEach(() => {
+    cy.request('POST', 'http://localhost:3000/login', 'testToken')
+    cy.visit('/')
+    cy.waitForReact()
+  })
+
+  it('should clear login cookie', () => {
+    cy.react('MenuButton').click()
+    cy.react('DrawerItem', { props: { label: 'Log out' } }).click()
+    cy.getCookie('jwt').should('have.property', 'value', '')
+  })
+
+  it('should redirect to login page', () => {
+    cy.react('MenuButton').click()
+    cy.react('DrawerItem', { props: { label: 'Log out' } }).click()
+    cy.url().should('include', 'login')
+  })
+})
