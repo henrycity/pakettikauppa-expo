58b44b8c80ee96efd14d66b0489ec66eb7272b10
Pyry Pohjalainen <pyry@MacBook-Pro.local>
2021-01-11 10:49:36 +0200

    renamed .env variables

--- src/config.ts
+++ src/config.ts
@@ -1,14 +1,18 @@
-import { EXPO_LOCAL_ADDRESS, EXPO_LOCAL_IP, EXPO_SERVER } from '@env'
+import {
+  EXPO_DEV_SERVER_WEB,
+  EXPO_DEV_SERVER_MOBILE,
+  EXPO_PROD_SERVER,
+} from '@env'
 import { Platform } from 'react-native'
 
 export default function server(): string {
   if (__DEV__) {
     if (Platform.OS === 'web') {
-      return EXPO_LOCAL_ADDRESS
+      return EXPO_DEV_SERVER_WEB
     } else {
-      return EXPO_LOCAL_IP
+      return EXPO_DEV_SERVER_MOBILE
     }
   } else {
-    return EXPO_SERVER
+    return EXPO_PROD_SERVER
   }
 }
