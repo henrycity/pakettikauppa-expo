4843d803b301b207cf09822ef30e4484aec73c99
Carl-Victor Schauman <carl-victor.schauman@aalto.fi>
2021-01-20 11:19:31 +0200

    PA-3-A: Fixed some linting errors

--- src/modules/shipments/getShipmentData.ts
+++ src/modules/shipments/getShipmentData.ts
@@ -1,7 +1,7 @@
 import server from '../../config'
 
-export default async function getShipmentData(): object[] {
-  let res
+export default async function getShipmentData(): Promise<object[]> {
+  let res: object[] = [{}]
   await fetch(`${server()}/shipments`)
     .then((response) => response.json())
     .then((data) => (res = data))
