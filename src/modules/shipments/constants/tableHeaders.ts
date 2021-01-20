72ff29c0cb595a8a8f005c49ec5523f685ebea57
Carl-Victor Schauman <carl-victor.schauman@aalto.fi>
2021-01-20 19:23:46 +0200

    PA-3-A: Restructured the code, switched to useSWR and, externalised table and headerdata

---
+++ src/modules/shipments/constants/tableHeaders.ts
@@ -0,0 +1,52 @@
+const tableHeaders = [
+  {
+    Header: 'ID',
+    accessor: 'id',
+  },
+  {
+    Header: 'Invoice Number',
+    accessor: 'invoiceNumber',
+  },
+  {
+    Header: 'Receiver Name',
+    accessor: 'receiverName',
+  },
+  {
+    Header: 'Post Code',
+    accessor: 'postCode',
+  },
+  {
+    Header: 'Post Office',
+    accessor: 'postOffice',
+  },
+  {
+    Header: 'Country Code',
+    accessor: 'countryCode',
+  },
+  {
+    Header: 'Receiver Email',
+    accessor: 'receiverEmail',
+  },
+  {
+    Header: 'Reference',
+    accessor: 'reference',
+  },
+  {
+    Header: 'Price',
+    accessor: 'price',
+  },
+  {
+    Header: 'Status',
+    accessor: 'status',
+  },
+  {
+    Header: 'Latest Event',
+    accessor: 'latestEvent',
+  },
+  {
+    Header: 'Created On',
+    accessor: 'createdOn',
+  },
+]
+
+export default tableHeaders
--- src/modules/shipments/getShipmentData.ts
+++
@@ -1,9 +0,0 @@
-import server from '../../config'
-
-export default async function getShipmentData(): Promise<object[]> {
-  let res: object[] = [{}]
-  await fetch(`${server()}/shipments`)
-    .then((response) => response.json())
-    .then((data) => (res = data))
-  return res
-}
